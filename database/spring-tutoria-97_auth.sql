0c1911b9b69e45ec151221b93496933f3847fac1
thinkerinhell <wuchen2010@gmail.com>
2016-07-21 22:15:00 +1000

    fix db script and add logging debug feature

--- database/spring-tutoria-97_auth.sql
+++ database/spring-tutoria-97_auth.sql
@@ -8,6 +8,6 @@ CREATE TABLE `users` (
   `username` varchar(60) NOT NULL,
   `password` varchar(80) DEFAULT NULL,
   `email` varchar(60) DEFAULT NULL,
-  `enabled` tinyint(1) DEFAULT '1',
+  `enabled` tinyint(1) DEFAULT 1,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
